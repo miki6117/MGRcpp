420e8631b6392a3c394b40c00c84c4de677639f8
MG <mg@mg.com>
2018-04-20 19:52:04 +0200

    Basic model of asic generator

--- datagen.cpp
+++ datagen.cpp
@@ -14,46 +14,57 @@ void DataGenerator::performActionOnGeneratedData(const unsigned char &data_char,
 
 void DataGenerator::asic()
 {
-	size_t asic_data_size = id * channel * 8;
-    // cout << "DEBUG: Asic data size: " << asic_data_size << endl;
-    unsigned char* asic_data = new unsigned char[asic_data_size];
-    uint16_t amplitude = 0x123;
-    unsigned long long int timestamp = 0;
-    
+	// size_t asic_data_size = id * channel * 8;
+	// cout << "DEBUG: Asic data size: " << asic_data_size << endl;
+	// unsigned char* asic_data = new unsigned char[asic_data_size];
+	// uint16_t amplitude = 0x123;
+	// unsigned long long int timestamp = 0;
+	
+	// uint8_t id; // 4b
+	// uint8_t channel; // 8b
+	uint16_t amplitude = 0x123; // 16b
+	uint64_t timestamp = 0; // 36b
 
-    unsigned int asic_data_position = 0;
-    for (uint8_t i = 1; i <= id; i++)
-    {
-        for (uint8_t j = 1; j <= channel; j++)
-        {
-            asic_data[asic_data_position] = static_cast<unsigned char>(i); // ID on first position
-            asic_data[asic_data_position] += static_cast<unsigned char>(j << 4); // half of CHANNEL on first position
-            asic_data[++asic_data_position] = static_cast<unsigned char>(j >> 4); // second half of CHANNEL on second position
-            // amplitude = (amplitude >> 1) | ((((amplitude >> 4)^(amplitude >> 10)^(amplitude >> 12)) & 1) << 15); // {amplitude[15:0], amplitude[11] ^ amplitude[5] ^ amplitude[3]}; so: x^12 + x^6 + x^4
-            amplitude = (amplitude << 1) | ((((amplitude >> 11)^(amplitude >> 5)^(amplitude >> 3)) & 1)); // {amplitude[15:0], amplitude[11] ^ amplitude[5] ^ amplitude[3]}; so: x^12 + x^6 + x^4
-            asic_data[asic_data_position] += static_cast<unsigned char>(amplitude << 4); // 4 bit part of AMPLIT on second position
-            asic_data[++asic_data_position] = static_cast<unsigned char>(amplitude >> 4); // 8 bit part of AMPLIT on third position
-            asic_data[++asic_data_position] = static_cast<unsigned char>(amplitude >> 12); // 4 bit part of AMPLIT on fourth position
+	uint8_t asic_data0; // ID and half of channel
+	uint8_t asic_data1; // second half of CHANNEL and 1/4 of AMPLITUDE
+	uint8_t asic_data2; // 1/2 of AMPLITUDE
+	uint8_t asic_data3; // 1/4 of AMPLITUDE and 1/9 of TIMESTAMP
 
-            asic_data[asic_data_position] += static_cast<unsigned char>(timestamp << 4);
-            asic_data[++asic_data_position] = static_cast<unsigned char>(timestamp >> 4);
-            asic_data[++asic_data_position] = static_cast<unsigned char>(timestamp >> 12);
-            asic_data[++asic_data_position] = static_cast<unsigned char>(timestamp >> 20);
-            asic_data[++asic_data_position] = static_cast<unsigned char>(timestamp >> 28);
-            ++asic_data_position;
-            cout << "ID: " << (int)i <<", CHANNEL: " << (int)j << ", AMPLITUDE: " << (int)amplitude << endl;
-            // asic_data[asic_data_position] = i + (j << 4); // half of CHANNEL on first position
-            // amplitude = (amplitude >> 1) | (((amplitude >> 4)^(amplitude >> 10)^(amplitude >> 12)) << 15); // {amplitude[15:0], amplitude[11] ^ amplitude[5] ^ amplitude[3]}; so: x^12 + x^6 + x^4
-            // asic_data[++asic_data_position] = static_cast<unsigned char>(j >> 4) + static_cast<unsigned char>(amplitude << 4); // 4 bit part of AMPLIT on second position
-            // asic_data[++asic_data_position] = static_cast<unsigned char>(amplitude >> 4); // 8 bit part of AMPLIT on third position
-            // asic_data[++asic_data_position] = static_cast<unsigned char>(amplitude >> 12) + static_cast<unsigned char>(timestamp << 4);
-            // asic_data[++asic_data_position] = static_cast<unsigned char>(timestamp >> 4);
-            // asic_data[++asic_data_position] = static_cast<unsigned char>(timestamp >> 12);
-            // asic_data[++asic_data_position] = static_cast<unsigned char>(timestamp >> 20);
-            // asic_data[++asic_data_position] = static_cast<unsigned char>(timestamp >> 28);
-            // ++asic_data_position;
-        }
-    }
+	uint8_t asic_data4; // 2/9 of TIMESTAMP
+	uint8_t asic_data5; // 2/9 of TIMESTAMP
+	uint8_t asic_data6; // 2/9 of TIMESTAMP
+	uint8_t asic_data7; // 2/9 of TIMESTAMP
+
+	uint8_t asic_data[8];
+
+	const uint8_t max_id = 15;
+	const uint8_t max_channel = 255;
+	const uint16_t max_amplitude = 65535;
+	const uint64_t max_timestamp = 68719476735;
+
+	for (uint8_t id = 1; id <= max_id; id++)
+	{
+		for (uint8_t channel = 1; channel <= max_channel; channel++)
+		{
+			amplitude = (amplitude << 1) | ((((amplitude >> 11)^(amplitude >> 5)^(amplitude >> 3)) & 1)); // {amplitude[15:0], amplitude[11] ^ amplitude[5] ^ amplitude[3]}; so: x^12 + x^6 + x^4
+			asic_data[0] = static_cast<unsigned char>(id);
+			asic_data[0] += static_cast<unsigned char>(channel << 4); // ID and half of channel
+
+			asic_data[1] = static_cast<unsigned char>(channel >> 4);
+			asic_data[1] += static_cast<unsigned char>(amplitude << 4); // second half of CHANNEL and 1/4 of AMPLITUDE
+
+			asic_data[2] = static_cast<unsigned char>(amplitude >> 4); // 1/2 of AMPLITUDE
+
+			asic_data[3] = static_cast<unsigned char>(amplitude >> 12);
+			asic_data[3] += static_cast<unsigned char>(timestamp << 4); // 1/4 of AMPLITUDE and 1/9 of TIMESTAMP
+
+			asic_data[4] = static_cast<unsigned char>(timestamp >> 4);  // 2/9 of TIMESTAMP
+			asic_data[5] = static_cast<unsigned char>(timestamp >> 12); // 2/9 of TIMESTAMP
+			asic_data[6] = static_cast<unsigned char>(timestamp >> 20); // 2/9 of TIMESTAMP
+			asic_data[7] = static_cast<unsigned char>(timestamp >> 28); // 2/9 of TIMESTAMP
+
+		}
+	}
 }
 
 void DataGenerator::walking1()
